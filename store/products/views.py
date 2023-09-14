ad09e1c6364f41a7585f7618ea5ad3210938758b
AntonG85 <ahtoh231b@gmail.com>
2023-09-14 22:50:30 +0300

    models creation and DB connection

--- store/products/views.py
+++ store/products/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from store.wsgi import *
+from products.models import ProductCategory, Product
 
 # Create your views here.
 
@@ -7,27 +9,10 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 def products(request):
+
     context = {
-        'products' : [
-            { 'name' : 'Худи черного цвета с монограммами adidas Originals',
-            'image' : '/static/vendor/img/products/Adidas-hoodie.png',
-            'price' : 6090,
-            'description' : 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.', },
-
-            { 'name' : 'Синяя куртка The North Face',
-            'image' : '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
-            'price' : 23725,
-            'description' : 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.', },
-
-            { 'name' : 'Черный рюкзак Nike Heritage',
-            'image' : '/static/vendor/img/products/Black-Nike-Heritage-backpack.png',
-            'price' : 2340,
-            'description' : 'Плотная ткань. Легкий материал.', },
-
-            { 'name' : 'Коричневый спортивный oversized-топ ASOS DESIGN',
-            'image' : '/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
-            'price' : 3390,
-            'description' : 'Материал с плюшевой текстурой. Удобный и мягкий.', },
-                    ],
+        'title' : 'Продукты',
+        'products' : Product.objects.all(),
+        'categorys' : ProductCategory.objects.all(),
     }
     return render(request, 'products/products.html', context)
