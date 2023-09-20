7f7764a089838361c7a34a8a04adf3b47a63b3f1
AntonG85 <ahtoh231b@gmail.com>
2023-09-20 20:15:28 +0300

    add login form

--- store/settings.py
+++ store/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'products',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# users
+AUTH_USER_MODEL = 'users.User'
---
+++ users/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate
+
+from users.forms import UserLoginForm
+from users.models import User
+
+# Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
+
+def registration(request):
+    return render(request, 'users/registration.html')
