6e8065eeb60a080b7ed64f57167b47f275691483
AntonG85 <ahtoh231b@gmail.com>
2023-10-04 22:11:28 +0300

    apply CBV for index, products, registration and profile page

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from users.views import login, logout, registration, profile
+from users.views import login, logout, UserRegistrationView, UserProfileView
+from django.contrib.auth.decorators import login_required
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
-    path('registration/', registration, name='registration'),
-    path('profile/', profile, name='profile'),
+    path('registration/', UserRegistrationView.as_view(), name='registration'),
+    path('profile/<int:pk>/', login_required(UserProfileView.as_view()), name='profile'),
     ]
